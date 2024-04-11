1968110baef68baf5b0d76995b14456f9bad7ee4
Kribbany <palyi.kristof@gmail.com>
2024-04-11 22:15:09 +0200

    doga instrukciok

---
+++ 11c/13_2024_04_12/main_raw.py
@@ -0,0 +1,12 @@
+# prompt: Tudnivalók a dogával kapcsolatban:
+# - Most könnyebb lesz sokkal, mint az előző dogában.
+# - Kapni fogtok egy feladatlapot, amin lesznek gyakorló feladatok.
+# - A feladatlapon szereplő feladatokhoz hasonlók lesznek a dogában is
+# ! NEm ugyanolyanok ne tanuljátok meg őket!
+# - A doga során nem használhattok internetet, jegyzeteket, semmilyen segédeszközt.
+# - Cserébe tényleg nagyon basic lesz.abs
+# - Javításnál a nem lefutó program 0 pontot ér.
+# - 6 feladat lesz, mindegyik 1 jegyet ér. (1-et még el lehet cseszni hogy 5-ös legyen.)
+# - Ha egy feladat nem megy, ne ragadjatok rajta sokáig, inkább ugorjatok tovább.
+# - Ha egy feladat gátolja a futást, inkább kommentezzétek ki, hogy a többi futni tudjon.
+# - A feladatokat a megadott sorrendben szerepeljenek a fájlban, mert úgy fogom írni a teszteket.
